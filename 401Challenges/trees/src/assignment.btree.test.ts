265817a3e6d14d812223f85c398fade3f4899db6
Jim Doyle <jamesdoyle202@gmail.com>
2022-09-15 17:45:36 -0700

    CC18 complete

--- 401Challenges/trees/src/assignment.btree.test.ts
+++ 401Challenges/trees/src/assignment.btree.test.ts
@@ -1,4 +1,4 @@
-const { BinaryTree, Node, breadthFirst } = require("./binary_tree.ts");
+const { BinaryTree, Node, breadthFirst, fizzBuzzTree, fizzBuzzNode } = require("./binary_tree.ts");
 
 describe("Binary Tree", () => {
   const tree = new BinaryTree(
@@ -8,7 +8,14 @@ describe("Binary Tree", () => {
       new Node(9, undefined, new Node(9, new Node(5)))
     )
   );
-// 
+  const fizzTree = new BinaryTree(
+    new Node(
+      '1',
+      new Node('7', new Node('2'), new Node('Fizz', new Node('Fizz'), new Node('11'))),
+      new Node('Fizz', undefined, new Node('Fizz', new Node('Buzz')))
+    )
+  )
+  // 
   //          1
   //        /   \
   //       7     9
@@ -16,9 +23,10 @@ describe("Binary Tree", () => {
   //     2   6     9
   //        / \   /
   //       3  11 5
-  it("does an in-order traversal (left, root, right)", () => {
+  it("does an in-order traversal as a method on the class (left, root, right)", () => {
     expect(tree.inOrder()).toEqual([2, 7, 3, 6, 11, 1, 9, 5, 9]);
   });
+
 
   it("does an post-order traversal (left, right, root)", () => {
     expect(tree.postOrder()).toEqual([2, 3, 11, 6, 7, 5, 9, 9, 1]);
@@ -28,11 +36,21 @@ describe("Binary Tree", () => {
     expect(tree.preOrder()).toEqual([1, 7, 2, 6, 3, 11, 9, 9, 5]);
   });
 
-  it("returns the maximum value stored in the tree", ()=>{
+  it("returns the maximum value stored in the tree", () => {
     expect(tree.max()).toEqual(11);
   })
-  it("returns a list of all values in the tree, left to right, gen 0 to gen n", ()=>{
-    expect(breadthFirst(tree)).toEqual([1,7,9,2,6,9,3,11,5]);
+  it("returns a list of all values in the tree, left to right, gen 0 to gen n", () => {
+    expect(breadthFirst(tree)).toEqual([1, 7, 9, 2, 6, 9, 3, 11, 5]);
+  })
+  it('can fizzBuzz a node', () => {
+    expect(fizzBuzzNode(new Node(1, undefined, undefined))).toEqual('1');
+    expect(fizzBuzzNode(new Node(3, undefined, undefined))).toEqual('Fizz');
+    expect(fizzBuzzNode(new Node(5, undefined, undefined))).toEqual('Buzz');
+    expect(fizzBuzzNode(new Node(15, undefined, undefined))).toEqual('FizzBuzz');
+
+  })
+  it("Can fizz buzz a tree", () => {
+    expect(fizzBuzzTree(tree)).toEqual(fizzTree);
   })
 });
-export {};
+export { };
