8ae8e7bdebf7488c2da0c931b0503932a77bd668
Jim Doyle <jamesdoyle202@gmail.com>
2022-10-08 14:56:05 -0700

    CC complete

---
+++ 401Challenges/selection-sort/src/merge_sort/merge_sort.test.ts
@@ -0,0 +1,13 @@
+const {mergeSort} = require('./merge_sort.ts')
+
+describe("mergeSort", ()=>{
+  const A = [8,4,23,42,16,15];
+  const B = [];
+  const C = [1];
+
+  it("sorts the array", ()=>{
+    expect(mergeSort(A)).toEqual([4,8,15,16,23,42]);
+    expect(mergeSort(B)).toEqual([]);
+    expect(mergeSort(C)).toEqual([1]);
+  })
+})
